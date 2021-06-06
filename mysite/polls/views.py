d76357c0200e4d87384866a8d1336691e246a00f
OPM4 <mateioprea@gmail.com>
2021-06-06 19:40:15 +0300

    raising a 404 error

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -9,15 +9,18 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    
     context = {
         'latest_question_list': latest_question_list,
             }
-    
     return render(request, 'polls/index.html', context)
     
 def details(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+    return render(request, 'polls/details.html', {'question': question})
+    # return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
