76a8cc931b3359931777a84e1a9b999db6598d92
OPM4 <mateioprea@gmail.com>
2021-06-06 19:32:56 +0300

    polls/views cleanup

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,6 @@
 from django import template
 from django.shortcuts import render
-from django.http import HttpResponse, response
+from django.http import HttpResponse, response, Http404
 from django.template import context, loader
 
 from .models import Question
@@ -9,16 +9,13 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # template = loader.get_template('polls/index.html')
+    
     context = {
         'latest_question_list': latest_question_list,
             }
-    # output = ', '.join([q.question_text for q in latest_question_list])
+    
     return render(request, 'polls/index.html', context)
-    # return HttpResponse(template.render(context, request))
-    # return HttpResponse(output)
-    # return HttpResponse("Hello World. You're at the polls index.")
-
+    
 def details(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
