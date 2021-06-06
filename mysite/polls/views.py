4f9e685a24181c9aa225f4c74609ddfd09c2990a
OPM4 <mateioprea@gmail.com>
2021-06-06 19:09:41 +0300

    views that do something

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,23 @@
+from django import template
 from django.shortcuts import render
 from django.http import HttpResponse, response
+from django.template import context, loader
+
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+            }
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    return render(request, 'polls/index.html', context)
+    # return HttpResponse(template.render(context, request))
+    # return HttpResponse(output)
+    # return HttpResponse("Hello World. You're at the polls index.")
 
 def details(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
